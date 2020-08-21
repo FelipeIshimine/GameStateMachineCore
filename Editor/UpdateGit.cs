2c06c53a928182aa001ddae59481ba10bb5142fc
Felipe Ishimine <felipeishimine@gmail.com>
2020-08-21 10:52:25 -0300

    1.0.6 HOTFIX
    -Arreglado error durante proceso de BUILD, debido a que las Assembly de Editor no estaba correctamente separada

--- Editor/UpdateGit.cs
+++ Editor/UpdateGit.cs
@@ -7,7 +7,7 @@ using UnityEditor.PackageManager.Requests;
 
 public static class UpdateGit 
 {
-    [MenuItem("GameStateMachine/UpdatePackage")]
+    [MenuItem("GameStateMachine/UpdatePackage")]    
     public static void SelectMe()
     {
         AddRequest request = Client.Add("https://github.com/FelipeIshimine/GameStateMachineCore.git");
